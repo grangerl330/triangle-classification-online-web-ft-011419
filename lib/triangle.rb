1ed53da633f776bfacdfebb527b1dec17416882d
Luke Granger <grangerl330@gmail.com>
2019-02-02 01:29:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,9 @@ class Triangle
   end 
   
   def kind 
-    if side_1 <= 0 || side_2 <= 0 || side_3 <= 0 ||  
+    if side_1 <= 0 || side_2 <= 0 || side_3 <= 0 || side_1 + side_2 <= side_3 || side_2 + side_3 <= side_1 || side_1 + side_3 <= side_2
+        raise TriangleError
+    elsif side_1 == side_2 
   
   class TriangleError < StandardError 
     "No side can be less than a length of 1 AND two sides added together must always be greater than the third side"
